801eca93097f45ecb470a0160dedba66f38ed54a
Cody Jones <Cody.Monroe.Jones@gmail.com>
2022-12-03 13:04:57 -0600

    started tab creation for windows

--- DBGui.py
+++ DBGui.py
@@ -1,10 +1,26 @@
 from tkinter import *
+from tkinter import ttk
 import sqlite3
 
 # Set the gui title and window
 root = Tk()
 root.title("DB Gui")
 root.geometry("400x400")
+
+#Tabbed window for different options
+tab_window = ttk.Notebook(root)
+
+# Setting a tab frame for each CSV file for basic command testing
+customer_tab = ttk.Frame(tab_window)
+rate_tab     = ttk.Frame(tab_window)
+rental_tab   = ttk.Frame(tab_window)
+vehicle_tab  = ttk.Frame(tab_window)
+
+# Adding the tabs to the GUI window
+ttk.Label(customer_tab, Text='Customers')
+ttk.Label(rate_tab,     Text='Rates')
+ttk.Label(rental_tab,   Text='Rentals')
+ttk.Label(vehicle_tab,  Text='Vehicles')
 
 #Setting the connection to the db
 conn = sqlite3.connect("")
