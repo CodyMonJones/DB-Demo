47cf4e7bcba7bbfd22b05ebe8730e93a32a46ed4
Cody Jones <cody.monroe.jones@gmail.com>
2022-12-04 10:38:42 -0600

    moved some variables out of functions to be seen -CJ

--- DBGui.py
+++ DBGui.py
@@ -11,39 +11,77 @@ root.geometry("400x400")
 #Tabbed window for different options
 tab_window = ttk.Notebook(root)
 
-def test_query():
+# Setting a tab frame for each CSV file for basic command testing
+query_tab1 = ttk.Frame(tab_window)
+query_tab2 = ttk.Frame(tab_window)
+query_tab3 = ttk.Frame(tab_window)
+query_tab4 = ttk.Frame(tab_window)
+
+# Adding the tabs to the GUI window
+tab_window.add(query_tab1, text='Query 1')
+tab_window.add(query_tab2, text='Query 2')
+tab_window.add(query_tab3, text='Query 3')
+tab_window.add(query_tab4, text='Query 4')
+tab_window.pack(expand= 1, fill="both")
+
+def submit_vehicle():
     try:
         conn = sqlite3.connect("carRental.db")
     except Error as error:
         print(error)
 
     cur = conn.cursor()
+    cur.execute("INSERT INTO VEHICLE VALUES (:vid, :description, :year, :type, :category)",
+    {
+        'vid': vid.get(),
+        'description': description.get(),
+        'year': year.get(),
+        'type': carType.get(),
+        'category': category.get()
+    })
 
-    cur.execute("SELECT * FROM CUSTOMER")
-    results = cur.fetchall()
-    print(results)
 
     conn.commit()
-    conn.close()
-        
+    conn.close()  
 
-def set_tabs():
-    # Setting a tab frame for each CSV file for basic command testing
-    query_tab1 = ttk.Frame(tab_window)
-    query_tab2 = ttk.Frame(tab_window)
-    query_tab3 = ttk.Frame(tab_window)
-    query_tab4 = ttk.Frame(tab_window)    
+# Vehicle information to add to DB
+#query 2 entry boxes and buttons
+vid_label = Label(query_tab2, text = 'Vehicle ID: ')
+vid_label.grid(row = 0, column  = 0)
 
-    # Adding the tabs to the GUI window
-    tab_window.add(query_tab1, text='Query 1')
-    tab_window.add(query_tab2, text='Query 2')
-    tab_window.add(query_tab3, text='Query 3')
-    tab_window.add(query_tab4, text='Query 4')
-    tab_window.pack(expand= 1, fill="both")
+vid = Entry(query_tab2, width = 30)
+vid.grid(row = 0, column = 1, padx = 20)
 
-    #Buttons for queries
-    test_query_btn = Button(query_tab1, text="Display all Customers", command=test_query)
-    test_query_btn.grid(row = 7, column=0, columnspan=2, padx=10, pady=10)
+description_label = Label(query_tab2, text = 'Description: ')
+description_label.grid(row = 1, column  = 0)
+
+description = Entry(query_tab2, width = 30)
+description.grid(row = 1, column = 1)
+
+
+year_label = Label(query_tab2, text = 'Year: ')
+year_label.grid(row = 2, column = 0)
+
+year = Entry(query_tab2, width = 30)
+year.grid(row = 2, column = 1)
+
+
+car_label = Label(query_tab2, text = 'Type: ')
+car_label.grid(row = 3, column = 0)
+
+carType = Entry(query_tab2, width = 30)
+carType.grid(row = 3, column = 1)
+
+
+category_label = Label(query_tab2, text = 'Category: ')
+category_label.grid(row = 4, column = 0)
+
+category = Entry(query_tab2, width = 30)
+category.grid(row = 4, column = 1)
+
+enterVehicle = Button(query_tab2, text = "Submit car", command = submit_vehicle)
+enterVehicle.grid(row = 5, column = 0, columnspan = 2, padx = 10, pady = 10)      
+
 
 def start_connection():
     #Setting the connection to the db file "carRental.db"
@@ -55,6 +93,6 @@ def start_connection():
 
 
 #Execute our window
-set_tabs()
+
 start_connection()
 root.mainloop()
