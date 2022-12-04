2a13f67a3efd5a8eb6730534e87350c38bf40181
Cody Jones <Cody.Monroe.Jones@gmail.com>
2022-12-04 17:17:27 -0600

    design changes -CJ

--- DBGui.py
+++ DBGui.py
@@ -17,12 +17,14 @@ query_tab1 = ttk.Frame(tab_window)
 query_tab2 = ttk.Frame(tab_window)
 query_tab3 = ttk.Frame(tab_window)
 query_tab4 = ttk.Frame(tab_window)
+query_tab5 = ttk.Frame(tab_window)
 
 # Adding the tabs to the GUI window
 tab_window.add(query_tab1, text='Query 1')
 tab_window.add(query_tab2, text='Query 2')
 tab_window.add(query_tab3, text='Query 3')
 tab_window.add(query_tab4, text='Query 4')
+tab_window.add(query_tab5, text='Query 5')
 tab_window.pack(expand=1, fill="both")
 
 #Tree view for displaying customer data
@@ -41,7 +43,7 @@ tree_view1.heading("3", text="Phone Number")
 
 #Tree view for displaying vehicle data
 tree_view2 = ttk.Treeview(query_tab2, selectmode='browse')
-tree_view2.grid(row=6, column=1, padx=20, pady=20)
+tree_view2.grid(row=11, column=0, padx=20, pady=20)
 tree_view2["columns"] = ("1", "2", "3", "4", "5")
 tree_view2["show"] = 'headings'
 tree_view2.column("1", width=200, anchor='w')
@@ -143,40 +145,40 @@ def show_vehicle_data():
 # Vehicle information to add to DB
 # query 2 entry boxes and buttons
 vid_label = Label(query_tab2, text='Vehicle ID: ')
-vid_label.grid(row=0, column=0)
+vid_label.grid(row=0, column=0, sticky=W)
 
 vid = Entry(query_tab2, width=30)
-vid.grid(row=0, column=1, padx=20)
+vid.grid(row=1, column=0, sticky=W, pady=(0, 10))
 
 description_label = Label(query_tab2, text='Description: ')
-description_label.grid(row=1, column=0)
+description_label.grid(row=2, column=0, sticky=W)
 
 description = Entry(query_tab2, width=30)
-description.grid(row=1, column=1)
+description.grid(row=3, column=0, sticky=W, pady=(0, 10))
 
 
 year_label = Label(query_tab2, text='Year: ')
-year_label.grid(row=2, column=0)
+year_label.grid(row=4, column=0, sticky=W)
 
 year = Entry(query_tab2, width=30)
-year.grid(row=2, column=1)
+year.grid(row=5, column=0, sticky=W, pady=(0, 10))
 
 
 car_label = Label(query_tab2, text='Type: ')
-car_label.grid(row=3, column=0)
+car_label.grid(row=6, column=0, sticky=W)
 
 carType = Entry(query_tab2, width=30)
-carType.grid(row=3, column=1)
+carType.grid(row=7, column=0, sticky=W, pady=(0, 10))
 
 
 category_label = Label(query_tab2, text='Category: ')
-category_label.grid(row=4, column=0)
+category_label.grid(row=8, column=0, sticky=W)
 
 category = Entry(query_tab2, width=30)
-category.grid(row=4, column=1)
+category.grid(row=9, column=0, sticky=W, pady=(0, 10))
 
 enterVehicle = Button(query_tab2, text="Submit car", command=submit_vehicle)
-enterVehicle.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+enterVehicle.grid(row=10, column=0, columnspan=2, padx=100, pady=10, sticky=W)
 
 #Customer Tab
 cust_name_label = Label(query_tab1, text='Name: ')
@@ -192,7 +194,7 @@ customer_phone = Entry(query_tab1, width=30)
 customer_phone.grid(row=3, column=0, pady=(0,10))
 
 enter_customer = Button(query_tab1, text="Add Customer", command=add_customer)
-enter_customer.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky=W)
+enter_customer.grid(row=4, column=0, columnspan=2, padx=100, pady=10, sticky=W)
 
 def start_connection():
     # Setting the connection to the db file "carRental.db"
